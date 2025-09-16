9429c68c1e1aa79eb4826533215f7757c7ca5810
hchubaldo <39108735+hchubaldo@users.noreply.github.com>
2025-09-16 10:54:22 -0500

    changed int to float

--- assignment1.py
+++ assignment1.py
@@ -14,23 +14,7 @@ modelFit = model.fit(assign1_data_df_keep)
 future = modelFit.make_future_dataframe(periods=744, freq='h', include_history=False)
 forecast_test = modelFit.predict(future)
 
-pred = forecast_test['yhat'].tolist()
-pred = [round(num) for num in pred]
-
-pred = [int(num) for num in pred]
-
-'''def checkNumbers(pred):
-    for i in pred:
-        if not isinstance(i, (float, int)):
-            print(f'False at {i} which is a {type(i)}')
-    print(f'True for all {len(pred)} values')
-
-def test_valid_pred(pred):
-  assert (len(np.squeeze(pred))==744 and checkNumbers(np.squeeze(pred))), \
-    "Make sure your prediction consists of integers\nor floating point numbers, and is a list or array of 744\nfuture predictions!"
-
-
-print('Length of pred:', len(pred))
-checkNumbers(pred)
-
-test_valid_pred(pred)'''
+# change pred to np array
+pred = np.array(forecast_test['yhat'])
+# round pred to nearest integer
+pred = np.round(pred).astype(float)
